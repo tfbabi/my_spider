8a65b0a009689231a520088ed312dd0b18983bc0
yannis <tfbabi@yeah.net>
2017-06-29 16:32:11 +0800

    Create meizitu_img.py

---
+++ meizitu_img.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+#encoding:utf8
+
+import requests
+from bs4 import BeautifulSoup
+import os,re
+import urllib,urllib2
+head =  {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3018.3 Safari/537.36'}
+for x in range(1,70):
+    re_url="http://www.mmjpg.com/home/"+str(x)
+    request=urllib2.Request(re_url,headers=head)
+    r=urllib2.urlopen(request)
+    html= r.read()
+    Soup = BeautifulSoup(html,'lxml')
+#   print Soup.prettify()
+    li_list = Soup.find('div',class_='pic').find_all('span',class_='title')
+
+    for li in li_list:
+        title=li.get_text()
+        herf=li.a['href']
+        page_html = requests.get(herf, headers=head)
+        html_Soup = BeautifulSoup(page_html.text, 'lxml')
+        max_page = html_Soup.find('div',class_='page').find_all('a')[-2].get_text()
+        base_path='E:\mm'
+        end_path=path = title.strip()
+        os.mkdir(os.path.join(base_path,end_path))
+        os.chdir("E:\mm\\"+path)
+        for page in range(1,int(max_page)+1):
+            page_url = herf+'/'+str(page)
+            img_html = requests.get(page_url, headers=head)
+            img_Soup = BeautifulSoup(img_html.text, 'lxml')
+            img_url = img_Soup.find('div', class_='content').find('img')['src']
+            name = img_url.split('/')[-1].split('.')[0]
+            img = requests.get(img_url, headers=head)
+            f = open(name+'.jpg', 'ab')
+            f.write(img.content)
+            f.close()
+#            print img_url
+
+'''
+        print "---"*30
+        print "标题:",title
+        print "链接:",herf
+
+    print "================the next %s ==================" % x
+'''
